63d60ab64a7aacf1aa6ffd36fb85ff077084dae5
typhonrt <support@typhonrt.org>
2016-04-21 20:18:47 -0700

    Added JSPM dependency for testing.

---
+++ config.js
@@ -0,0 +1,18 @@
+/**
+ * Provides an empty SystemJS config.js which is only populated and used during testing.
+ */
+System.config({
+   defaultJSExtensions: true,
+   transpiler: "babel",
+   babelOptions: {
+      "optional": [
+         "runtime",
+         "optimisation.modules.system"
+      ]
+   },
+   paths: {
+   },
+
+   map: {
+   }
+});
