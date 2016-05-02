5654497a5bea6252cf3b00afca323856f07311b6
typhonrt <support@typhonrt.org>
2016-05-02 16:05:56 -0700

    0.0.2 release

--- test/fixture/src/TestExportNamed.js
+++ test/fixture/src/TestExportNamed.js
@@ -1,7 +1,19 @@
+/**
+ * An empty test class.
+ */
 export class NamedClass1 {}
 
+/**
+ * An empty test class.
+ */
 export class NamedClass2 {}
 
+/**
+ * An empty test function.
+ */
 export function namedFunction1() {}
 
+/**
+ * An empty test function.
+ */
 export function namedFunction2() {}
--- test/src/PluginTest.js
+++ test/src/PluginTest.js
@@ -1,5 +1,21 @@
+'use strict';
+
+import { assert } from 'chai';
+import fs         from 'fs-extra';
+import path       from 'path';
+
 /**
+ * A very basic test to verify that the navigation has been replaced for `./test/fixture/docs/index.html`.
  */
 describe('PluginTest', () =>
 {
+   it('Navigation Replacement', () =>
+   {
+      const filePath = path.resolve(__dirname, `../fixture/docs/index.html`);
+      const indexHTML = fs.readFileSync(filePath, 'utf-8');
+
+      assert(indexHTML.includes('<input type="checkbox" name="group-0" id="group-0" checked><label for="group-0" class="nav-header">Local Source</label>'));
+      assert(indexHTML.includes('<input type="checkbox" name="group-1" id="group-1"><label for="group-1" class="nav-header">JSPM Managed Source</label>'));
+      assert(indexHTML.includes('<input type="checkbox" name="folder-1" id="folder-1" checked><label for="folder-1" class="nav-dir-path" data-ice="dirPath" data-scm-link="https://github.com/typhonjs-backbone-parse/backbone-parse-es6/tree/master/src" data-scm-type="github">src</label>'));
+   });
 });
